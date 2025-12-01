3862acdcf850817e45462f62cdfde59a19ebe9e9
Rahul kumar <157143602+Rahulkum67@users.noreply.github.com>
2025-12-01 22:21:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,36 +1,15 @@
 class Solution {
-    public static int[] margearray(int[] x,int[] y){
-        int i=0,j=0,k=0;
-        int arr[]=new int[x.length+y.length];
-        while(i<x.length && j< y.length){
-            if(x[i]<y[j]){
-                arr[k++]=x[i++];
-            }
-            else{
-                arr[k++]=y[j++];
-            }
-        }
-        while(i<x.length){
-            arr[k++]=x[i++];
-        }
-        while(j<y.length){
-            arr[k++]=y[j++];
-        }
-        return arr;
-    }
-    public static double margearraymedian(int[] x,int[] y){
-        int[] arr=margearray(x,y);
-        int n=arr.length;
-        if (n % 2 == 0) {
-            return (arr[n / 2 - 1] + arr[n / 2]) / 2.0; 
-        } else {
-            return arr[n / 2]; 
-        }
-    }
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        double a=margearraymedian(nums1,nums2);
-        return a;
-        
+        int n=nums1.length;
+        int m=nums2.length;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums1[i];
+        }
+        for(int i=0;i<m;i++){
+            sum+=nums2[i];
+        }
+        double ans= (double)sum/(n+m);
+        return ans;
     }
-
 }
