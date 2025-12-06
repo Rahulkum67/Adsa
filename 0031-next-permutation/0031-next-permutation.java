b063ebac2a411b4012593f4b3406ed495c4af198
Rahul kumar <157143602+Rahulkum67@users.noreply.github.com>
2025-12-06 14:17:05 +0530

    Time: 1 ms (12.15%), Space: 44.4 MB (46.79%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -2,44 +2,33 @@ class Solution {
     public void nextPermutation(int[] nums) {
         int n = nums.length;
         int index = -1;
-
-        // 1. Find the first decreasing element from the right
-        for (int i = n - 2; i >= 0; i--) {
-            if (nums[i] < nums[i + 1]) {
-                index = i;
+        for(int i=n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                index=i;
                 break;
             }
         }
-
-        // If no such element found → array is in descending order
-        if (index == -1) {
-            reverse(nums, 0, n - 1);
+        if(index==-1){
+            reversearr(nums,0,n-1);
             return;
         }
-
-        // 2. Find the next greater element on right side of index
-        for (int i = n - 1; i > index; i--) {
-            if (nums[i] > nums[index]) {
-                swap(nums, i, index);
+        for(int i=n-1;i>index;i--){
+            if(nums[i]>nums[index]){
+                int temp=nums[i];
+                nums[i]=nums[index];
+                nums[index]=temp;
                 break;
             }
         }
-
-        // 3. Reverse the right half to get the smallest ordering
-        reverse(nums, index + 1, n - 1);
+        reversearr(nums,index+1,n-1);
     }
-
-    private void swap(int[] nums, int i, int j) {
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-
-    private void reverse(int[] nums, int i, int j) {
-        while (i < j) {
-            swap(nums, i, j);
-            i++;
-            j--;
+    public void reversearr(int[] arr,int st,int end){
+        while(st<=end){
+            int temp=arr[st];
+            arr[st]=arr[end];
+            arr[end]=temp;
+            st++;
+            end--;
         }
     }
 }
