8c08c44b203c06f58ba8beee886776a28f15cba6
Rahul kumar <157143602+Rahulkum67@users.noreply.github.com>
2025-12-08 11:06:30 +0530

    Time: 4 ms (66.91%), Space: 46.5 MB (26.97%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.java
+++ 0013-roman-to-integer/0013-roman-to-integer.java
@@ -1,8 +1,7 @@
 class Solution {
     public int romanToInt(String s) {
         int n=s.length();
-        int sum=0;
-        Map<Character ,Integer> map=new HashMap<>();
+        HashMap<Character,Integer> map=new HashMap<>();
         map.put('I',1);
         map.put('V',5);
         map.put('X',10);
@@ -10,15 +9,16 @@ class Solution {
         map.put('C',100);
         map.put('D',500);
         map.put('M',1000);
+        int sum=0;
         for(int i=0;i<n;i++){
             int curr=map.get(s.charAt(i));
-            int next=(i+1<n) ? map.get(s.charAt(i+1)):0;
-            if(curr <next){
+            int next=(i+1<n) ? map.get(s.charAt(i+1)) :0;
+            if(curr<next){
                 sum-=curr;
             }else{
                 sum+=curr;
             }
         }
-        return sum;     
+        return sum;
     }
 }
