26687fc6a1f3dd64f048babf06351748b3b832a6
Rahul kumar <157143602+Rahulkum67@users.noreply.github.com>
2025-12-20 21:04:02 +0530

    Time: 20 ms (52.91%), Space: 45.6 MB (60.7%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,32 +1,44 @@
+
+
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        int n=nums.length;
-        List<List<Integer>> arr=new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        int n = nums.length;
+        if (n < 4) return res;
+
         Arrays.sort(nums);
-        if (n < 4) return arr;
-        for(int i=0;i<n-3;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            for(int j=i+1;j<n-2;j++){
-                if(j>i+1 && nums[j]==nums[j-1]) continue;
-                int left=j+1;
-                int right=n-1;
-                while(left<right){
-                    int sum=nums[i]+nums[j]+nums[left]+nums[right];
-                    if(sum==target){
-                        arr.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
+
+        for (int i = 0; i < n - 3; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            for (int j = i + 1; j < n - 2; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
+
+                int left = j + 1, right = n - 1;
+
+                while (left < right) {
+                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
+
+                    if (sum == target) {
+                        res.add(Arrays.asList(
+                            nums[i], nums[j], nums[left], nums[right]
+                        ));
+
+                        while (left < right && nums[left] == nums[left + 1]) left++;
+                        while (left < right && nums[right] == nums[right - 1]) right--;
+
                         left++;
                         right--;
-                        while (left < right && nums[left] == nums[left - 1]) left++;
-                        while (left < right && nums[right] == nums[right + 1]) right--;
-                    }else if(sum < target){
+                    } 
+                    else if (sum < target) {
                         left++;
-                    }else{
+                    } 
+                    else {
                         right--;
                     }
                 }
             }
         }
-        return arr;
-        
+        return res;
     }
 }
