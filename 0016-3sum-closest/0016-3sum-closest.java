d3034aba0f10a6072eebf5c0643927e8a4d1d51b
Rahul kumar <157143602+Rahulkum67@users.noreply.github.com>
2025-12-22 09:20:23 +0530

    Time: 17 ms (64.62%), Space: 45.5 MB (34.45%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,43 +1,26 @@
-import java.util.Arrays;
-
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int n = nums.length;
-
-        int closestSum = nums[0] + nums[1] + nums[2];
-
-        for (int i = 0; i < n - 2; i++) {
-            // \U0001f539 Skip duplicate elements (minor optimization)
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
-            int left = i + 1;
-            int right = n - 1;
-
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
-
-                // \U0001f539 Update if closer
-                if (Math.abs(sum - target) < Math.abs(closestSum - target)) {
-                    closestSum = sum;
+        int closestsum=nums[0]+nums[1]+nums[2];
+        int n=nums.length;
+        for(int i=0;i<n-2;i++){
+            int left=i+1;
+            int right=n-1;
+            while(left<right){
+                int sum=nums[i]+nums[left]+nums[right];
+                if(Math.abs(target-sum)<Math.abs(target-closestsum)){
+                    closestsum=sum;
                 }
-
-                // \U0001f539 Move pointers smartly
-                if (sum < target) {
-                    left++;
-                    // Skip duplicates on the left
-                    while (left < right && nums[left] == nums[left - 1]) left++;
-                } else if (sum > target) {
-                    right--;
-                    // Skip duplicates on the right
-                    while (left < right && nums[right] == nums[right + 1]) right--;
-                } else {
-                    // Exact match
+                if(sum==target){
                     return sum;
+                }else if(sum<target){
+                    left++;
+                }else{
+                    right--;
                 }
+
             }
         }
-
-        return closestSum;
+        return closestsum;
     }
 }
