fec428354596bea49a265da1dcf05320c28ab44c
Rahul kumar <157143602+Rahulkum67@users.noreply.github.com>
2026-01-10 22:31:25 +0530

    Time: 2 ms (98.72%), Space: 122.8 MB (25.15%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.java
@@ -4,9 +4,9 @@ class Solution {
         int first=Integer.MAX_VALUE;
         int second=Integer.MAX_VALUE;
         for(int i=0;i<n;i++){
-            if(first > nums[i]){
+            if(first >= nums[i]){
                 first=nums[i];
-            }else if(second > nums[i]){
+            }else if(second >= nums[i]){
                 second=nums[i];
             }else {
                 return true;
