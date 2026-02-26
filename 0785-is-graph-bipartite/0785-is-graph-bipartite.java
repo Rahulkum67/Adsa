5726856853ca6c0a2fc17ea94460fecc53085963
Rahul kumar <157143602+Rahulkum67@users.noreply.github.com>
2026-02-26 16:21:43 +0530

    Time: 2 ms (39.16%), Space: 47.1 MB (78.13%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,42 +1,36 @@
 class Solution {
     public boolean isBipartite(int[][] edges) {
         int V=edges.length;
-        ArrayList<ArrayList<Integer>> adj=new ArrayList<>();
-        for(int i=0;i<V;i++){
-            adj.add(new ArrayList<>());
-        }
-        for(int i=0;i<edges.length;i++){
-            int from=edges[i][0];
-            int to=edges[i][1];
-            adj.get(from).add(to);
-            adj.get(to).add(from);
-        }
+        
         Queue<Integer> q=new LinkedList<>();
         char[] iscolor=new char[V];
-        q.add(0);
-        iscolor[0]='r';
-        boolean[] isvisted=new boolean[V];
-        isvisted[0]=true;
-        while(!q.isEmpty()){
-            int curr=q.poll();
-            for(int i=0;i<adj.get(curr).size();i++){
-                int nighbour=adj.get(curr).get(i);
-                if(isvisted[nighbour]==false){
-                    isvisted[nighbour]=true;
-                    if(iscolor[curr]=='r'){
-                    iscolor[nighbour]='b';
-                    }else{
-                    iscolor[nighbour]='r';
+        
+        
+        boolean[] isvisited=new boolean[V];
+        
+        for (int start = 0; start < V; start++) {
+            if (isvisited[start]) continue;
+
+            q.add(start);
+            isvisited[start] = true;
+            iscolor[start] = 'r';
+
+            while (!q.isEmpty()) {
+                int curr = q.poll();
+
+                for (int i = 0; i < edges[curr].length; i++) {
+                    int neighbour = edges[curr][i];
+
+                    if (!isvisited[neighbour]) {
+                        isvisited[neighbour] = true;
+                        iscolor[neighbour] = (iscolor[curr] == 'r') ? 'b' : 'r';
+                        q.add(neighbour);
+                    } else {
+                        if (iscolor[curr] == iscolor[neighbour])
+                            return false;
                     }
-                    q.add(nighbour);
                 }
-                else {
-                    if(iscolor[curr]==iscolor[nighbour]) return false;
-                }
-                
-
             }
-
         }
         return true;
     }
