56e479687fcdd29c8e0093fe0081fd963e2c7db2
Rahul kumar <157143602+Rahulkum67@users.noreply.github.com>
2026-03-10 14:27:05 +0530

    Time: 32 ms (33.06%), Space: 46.7 MB (82.26%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,58 @@
+class Solution {
+
+    class DSU {
+        int[] parent;
+        int[] rank;
+
+        DSU(int n) {
+            parent = new int[n];
+            rank = new int[n];
+
+            for(int i = 0; i < n; i++)
+                parent[i] = i;
+        }
+
+        int find(int x) {
+            if(parent[x] != x)
+                parent[x] = find(parent[x]);
+            return parent[x];
+        }
+
+        void union(int a, int b) {
+            int pa = find(a);
+            int pb = find(b);
+
+            if(pa == pb) return;
+
+            if(rank[pa] < rank[pb])
+                parent[pa] = pb;
+            else if(rank[pb] < rank[pa])
+                parent[pb] = pa;
+            else {
+                parent[pb] = pa;
+                rank[pa]++;
+            }
+        }
+    }
+
+    public int removeStones(int[][] stones) {
+        int n = stones.length;
+
+        DSU dsu = new DSU(n);
+
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                    dsu.union(i, j);
+                }
+            }
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i = 0; i < n; i++)
+            set.add(dsu.find(i));
+
+        return n - set.size();
+    }
+}
