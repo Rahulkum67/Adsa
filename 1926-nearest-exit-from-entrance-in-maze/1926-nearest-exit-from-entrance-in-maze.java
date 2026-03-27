f6bdf5172a65a0edbaa4f598079f83d7e086d908
Rahul kumar <157143602+Rahulkum67@users.noreply.github.com>
2026-03-27 10:54:12 +0530

    Time: 4 ms (97.56%), Space: 48.2 MB (79.73%) - LeetHub

--- 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
+++ 1926-nearest-exit-from-entrance-in-maze/1926-nearest-exit-from-entrance-in-maze.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     class Pair{
         int row;
         int col;
@@ -34,6 +34,51 @@ class Solution {
                     int newcol=cuc+cd[j];
                     if(isvalid(newrow,newcol,n,m) && !isvisit[newrow][newcol] && maze[newrow][newcol]=='.'){
                         isvisit[newrow][newcol]=true;
+                        q.add(new Pair(newrow,newcol));
+                    }
+                }
+            }
+            ans++;
+        }
+        return -1;
+    }
+}*/
+class Solution {
+    class Pair{
+        int row;
+        int col;
+        Pair(int row,int col){
+            this.row=row;
+            this.col=col;
+        }
+    }
+    int[] rd={-1,1,0,0};
+    int[] cd={0,0,-1,1};
+    boolean isvalid(int x,int y,int n,int m){
+        return (x >= 0 && x<n && y>=0 && y<m);
+    }
+    public int nearestExit(char[][] maze, int[] entrance) {
+        int ans=0;
+        int n=maze.length;
+        int m=maze[0].length;
+        
+        Queue<Pair> q=new LinkedList<>();
+        q.add(new Pair(entrance[0],entrance[1]));
+        maze[entrance[0]][entrance[1]]='+';
+        while(!q.isEmpty()){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                Pair curr=q.poll();
+                int cur=curr.row;
+                int cuc=curr.col;
+                if((cur==0 || cur==n-1 || cuc ==0 || cuc==m-1)&& !(cur==entrance[0] && cuc==entrance[1])){
+                    return ans;
+                }
+                for(int j=0;j<4;j++){
+                    int newrow=cur+rd[j];
+                    int newcol=cuc+cd[j];
+                    if(isvalid(newrow,newcol,n,m) &&  maze[newrow][newcol]=='.'){
+                        maze[newrow][newcol]='+';
                         q.add(new Pair(newrow,newcol));
                     }
                 }
