ab57207b622ab4fc6cc38c742524b394baf07e9d
Rahul kumar <157143602+Rahulkum67@users.noreply.github.com>
2026-04-28 15:08:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -2,7 +2,7 @@ class Solution {
     public int coinChange(int[] coins, int amount) {
         int[] dp = new int[amount + 1];
 
-        // Fill with a large value
+       
         Arrays.fill(dp, amount + 1);
 
         dp[0] = 0;
