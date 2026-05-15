271c856c89dbdd476f01ae83ec9019edfee6d926
Rahul kumar <157143602+Rahulkum67@users.noreply.github.com>
2026-05-15 11:08:54 +0530

    Time: 2 ms (100%), Space: 46.3 MB (77.23%) - LeetHub

---
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -0,0 +1,31 @@
+class Solution {
+    boolean dfs(int[][] grid,int i,int j,int n,int m){
+        if(i<0 || j<0 || i>=n || j>=m){
+            return false;
+        }
+        if(grid[i][j]==1){
+            return true;
+        }
+        grid[i][j]=1;
+        boolean up=dfs(grid,i+1,j,n,m);
+        boolean down=dfs(grid,i-1,j,n,m);
+        boolean left=dfs(grid,i,j+1,n,m);
+        boolean right=dfs(grid,i,j-1,n,m);
+        return (up && down && left && right);
+    }
+    public int closedIsland(int[][] grid) {
+        int n=grid.length;
+        int m=grid[0].length;
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==0){
+                    if(dfs(grid,i,j,n,m)){
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+}
