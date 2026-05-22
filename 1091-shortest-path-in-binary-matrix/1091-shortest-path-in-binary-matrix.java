b7e16c2011685db2b1cb951505fa4170c8e692f4
Rahul kumar <157143602+Rahulkum67@users.noreply.github.com>
2026-05-22 11:48:25 +0530

    Time: 12 ms (95.57%), Space: 47.4 MB (86.75%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.java
@@ -6,49 +6,42 @@ class Solution {
             this.row=row;
             this.col=col;
         }
-
     }
-    int[] rd={-1,-1,-1,0,0,1,1,1};
-    int[] cd={-1, 0, 1, -1, 1, -1, 0, 1};
     boolean isvalid(int x,int y,int n,int m){
-        return (x>=0 && x<n && y>=0 && y<m);
+        return (x>=0 && y>=0 && x<n && y<m);
     }
     public int shortestPathBinaryMatrix(int[][] grid) {
         int n=grid.length;
-        int m=grid[0].length;
-        int count=0;
-        if(grid[0][0] != 0  ){
+        if (grid[0][0] == 1 || grid[n - 1][n - 1] == 1) {
             return -1;
         }
         Queue<Pair> q=new LinkedList<>();
         q.add(new Pair(0,0));
+        grid[0][0]=1;
+        int count=1;
         
-        count=1;
-        boolean[][] isvisted=new boolean[n][n];
-        isvisted[0][0]=true;
-        
+        int[] rd = {-1, -1, -1, 0, 0, 1, 1, 1};
+        int[] cd = {-1, 0, 1, -1, 1, -1, 0, 1};
         while(!q.isEmpty()){
-            for(int j=0;j<q.size();j++){
-            Pair curr=q.poll();
-            int cpr=curr.row;
-            int cpc=curr.col;
-            if(cpr==n-1 && cpc ==n-1){
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                Pair curr=q.poll();
+                int cpr=curr.row;
+                int cpc=curr.col;
+                if(cpr==n-1 && cpc ==n-1){
                     return count;
                 }
-            for(int i=0;i<8;i++){
-                int nr=cpr+rd[i];
-                int nc=cpc+cd[i];
-                
-                if(isvalid(nr,nc,n,n) && grid[nr][nc]==0  && isvisted[nr][nc]==false) {
-                    
-                    isvisted[nr][nc]=true;
-                    q.add(new Pair(nr,nc));
-                    
+                for(int j=0;j<8;j++){
+                    int nr=cpr+rd[j];
+                    int nc=cpc+cd[j];
+                    if(isvalid(nr,nc,n,n) && grid[nr][nc]==0){
+                        grid[nr][nc]=1;
+                        q.add(new Pair(nr,nc));
+                    }
                 }
-            }
             }
             count++;
         }
-        return-1;
+        return -1;
     }
 }
