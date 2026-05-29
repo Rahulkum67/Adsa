c518f73727a3d1d379156212710ea93ccf82b59d
Rahul kumar <157143602+Rahulkum67@users.noreply.github.com>
2026-05-29 11:11:29 +0530

    Time: 57 ms (50.57%), Space: 80.8 MB (63.74%) - LeetHub

--- 1345-jump-game-iv/1345-jump-game-iv.java
+++ 1345-jump-game-iv/1345-jump-game-iv.java
@@ -1,56 +1,42 @@
-import java.util.*;
-
 class Solution {
     public int minJumps(int[] arr) {
-        int n = arr.length;
-        if(n == 1) return 0;
-
-        Map<Integer, List<Integer>> map = new HashMap<>();
-
-        for(int i = 0; i < n; i++){
-            map.computeIfAbsent(arr[i], k -> new ArrayList<>()).add(i);
+        int n=arr.length;
+        Map<Integer , List<Integer>> map=new HashMap<>();
+        for(int i=0;i<n;i++){
+            map.computeIfAbsent(arr[i],k-> new ArrayList<>()).add(i);
         }
-
-        Queue<Integer> q = new LinkedList<>();
-        boolean[] visited = new boolean[n];
-
+        Queue<Integer> q=new LinkedList<>();
+        boolean[] isv=new boolean[n];
         q.add(0);
-        visited[0] = true;
-
-        int steps = 0;
-
+        isv[0]=true;
+        int step=0;
         while(!q.isEmpty()){
-            int size = q.size();
+            int size=q.size();
+            for(int i=0;i<size;i++){
+                int curr=q.poll();
+                if(curr==n-1) return step;
 
-            for(int i = 0; i < size; i++){
-                int curr = q.poll();
-
-                if(curr == n - 1) return steps;
-
-                if(curr - 1 >= 0 && !visited[curr - 1]){
-                    visited[curr - 1] = true;
-                    q.add(curr - 1);
+                if(curr-1 >= 0 && isv[curr-1]==false){
+                    isv[curr-1]=true;
+                    q.add(curr-1);
                 }
 
-                if(curr + 1 < n && !visited[curr + 1]){
-                    visited[curr + 1] = true;
-                    q.add(curr + 1);
+                if(curr+1 < n && isv[curr+1]==false){
+                    isv[curr+1]=true;
+                    q.add(curr+1);
                 }
-
                 if(map.containsKey(arr[curr])){
-                    for(int next : map.get(arr[curr])){
-                        if(!visited[next]){
-                            visited[next] = true;
+                    for(int next:map.get(arr[curr])){
+                        if(!isv[next]){
+                            isv[next]=true;
                             q.add(next);
                         }
                     }
-                    map.remove(arr[curr]); 
+                    map.remove(arr[curr]);
                 }
             }
-
-            steps++;
+            step++;
         }
-
         return -1;
     }
 }
