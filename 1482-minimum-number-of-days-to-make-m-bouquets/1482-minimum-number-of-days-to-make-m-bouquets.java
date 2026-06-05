d2210bff73e5234bae1ccfcf086c9b5ffeabc4bf
Rahul kumar <157143602+Rahulkum67@users.noreply.github.com>
2026-06-05 12:08:08 +0530

    Time: 18 ms (53.86%), Space: 82.6 MB (63.65%) - LeetHub

---
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -0,0 +1,37 @@
+class Solution {
+    boolean ispossible(int mid,int[] arr,int m,int k){
+        int flower=0;
+        int bqt=0;
+        for(int ar:arr){
+            if(ar<=mid){
+                flower++;
+                if(flower==k){
+                    bqt++;
+                    flower=0;
+                }
+            }else{
+                flower=0;
+            }
+        }
+        return (bqt>=m);
+    }
+    public int minDays(int[] bloomDay, int m, int k) {
+        int st=0;
+        int end=0;
+        for(int i=0;i<bloomDay.length;i++){
+            st=Math.min(st,bloomDay[i]);
+            end=Math.max(end,bloomDay[i]);
+        }
+        int ans=-1;
+        while(st<=end){
+            int mid=st+(end-st)/2;
+            if(ispossible(mid,bloomDay,m,k)){
+                ans=mid;
+                end=mid-1;
+            }else{
+                st=mid+1;
+            }
+        }
+        return ans;
+    }
+}
