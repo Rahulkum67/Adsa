3bc98d2d8e7fdc64e6db35f6525c00f16e218c4b
Rahul kumar <157143602+Rahulkum67@users.noreply.github.com>
2026-07-14 12:29:12 +0530

    Time: 14 ms (70.91%), Space: 48.6 MB (91.8%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.java
+++ 0542-01-matrix/0542-01-matrix.java
@@ -1,8 +1,8 @@
 class Solution {
-    class pair{
+    class Pair{
         int row;
         int col;
-        pair(int row,int col){
+        Pair(int row,int col){
             this.row=row;
             this.col=col;
         }
@@ -13,28 +13,28 @@ class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int n=mat.length;
         int m=mat[0].length;
-        int[] rd={1,-1,0,0};
-        int[] cd={0,0,1,-1};
-        Queue<pair> q=new LinkedList<>();
+        Queue<Pair> q=new LinkedList<>();
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(mat[i][j]==0){
-                    q.add(new pair(i,j));
+                    q.add(new Pair(i,j));
                 }else{
                     mat[i][j]=-1;
                 }
             }
         }
+        int rd[]={-1,1,0,0};
+        int cd[]={0,0,-1,1};
         while(!q.isEmpty()){
-            pair curr=q.poll();
-            int cpr=curr.row;
-            int cpc=curr.col;
+            Pair cur=q.poll();
+            int cpr=cur.row;
+            int cpc=cur.col;
             for(int i=0;i<4;i++){
                 int nr=cpr+rd[i];
                 int nc=cpc+cd[i];
-                if(isvalid(nr,nc,n,m) && mat[nr][nc]== -1){
-                    mat[nr][nc]=1+mat[cpr][cpc];
-                    q.add(new pair(nr,nc));
+                if(isvalid(nr,nc,n,m) && mat[nr][nc]==-1){
+                    mat[nr][nc]=mat[cpr][cpc]+1;
+                    q.add(new Pair(nr,nc));
                 }
             }
         }
