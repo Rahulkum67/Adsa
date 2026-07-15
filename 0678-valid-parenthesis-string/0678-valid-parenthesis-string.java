3d7a43a45daccba5ec48b5307db8ead9b445900c
Rahul kumar <157143602+Rahulkum67@users.noreply.github.com>
2026-07-15 18:45:56 +0530

    Time: 0 ms (100%), Space: 42.9 MB (34.02%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        int low=0;
+        int high=0;
+        
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='('){
+                low++;
+                high++;
+
+            }else if(s.charAt(i)==')'){
+                low--;
+                high--;
+            }else{
+                low--;
+                high++;
+            }
+
+            if(high<0) return false;
+            low=Math.max(low,0);
+        }
+        return low==0;
+    }
+}
