49855c5668c93917c0e47f1b6f9281a44b6ba498
Rahul kumar <157143602+Rahulkum67@users.noreply.github.com>
2026-07-28 16:01:37 +0530

    Time: 0 ms (100%), Space: 42.1 MB (81.2%) - LeetHub

--- 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
+++ 1551-minimum-operations-to-make-array-equal/1551-minimum-operations-to-make-array-equal.java
@@ -1,6 +1,11 @@
 class Solution {
     public int minOperations(int n) {
         int mid=n/2;
-        return mid*(mid+n%2);
+        //return mid*(mid+n%2);
+        if(n%2==0){
+            return mid*mid;
+        }else{
+            return mid*(mid+1);
+        }
     }
 }
