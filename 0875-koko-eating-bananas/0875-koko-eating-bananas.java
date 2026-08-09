d36fa2cb905c62edd097c57470c756b544984d58
Rahul kumar <157143602+Rahulkum67@users.noreply.github.com>
2026-08-09 22:04:41 +0530

    Time: 6 ms (99.69%), Space: 47.6 MB (88.41%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,36 +1,30 @@
 class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
-        int low = 1;
-        int high = 0;
+    boolean canfinish(int[] arr,int h,int k){
+        int hour=0;
+        for(int a:arr){
+            hour+=(a+k-1)/k;
+            if(hour>h) return false;
 
-        // Find maximum pile
-        for (int pile : piles) {
-            high = Math.max(high, pile);
         }
-
-        int answer = high;
-
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-
-            if (canFinish(piles, h, mid)) {
-                answer = mid;      // possible speed
-                high = mid - 1;    // try smaller speed
-            } else {
-                low = mid + 1;     // need higher speed
+        return hour<=h;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int n=piles.length;
+        int low=1;
+        int high=0;
+        for(int pi:piles){
+            high=Math.max(high,pi);
+        }
+        
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(canfinish(piles,h,mid)){
+                
+                high=mid-1;
+            }else{
+                low=mid+1;
             }
         }
-
-        return answer;
-    }
-
-    private boolean canFinish(int[] piles, int h, int k) {
-        long hours = 0;
-
-        for (int pile : piles) {
-            hours += (pile + k - 1) / k; // ceil division
-        }
-
-        return hours <= h;
+        return low;
     }
 }
