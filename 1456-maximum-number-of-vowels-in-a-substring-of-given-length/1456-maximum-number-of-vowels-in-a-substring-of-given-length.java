11f80536b8f9e8bdefd7c52cfbed81a6b150abca
Rahul kumar <157143602+Rahulkum67@users.noreply.github.com>
2026-08-18 23:42:14 +0530

    Time: 22 ms (19.96%), Space: 46.1 MB (92.05%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -2,27 +2,21 @@ class Solution {
     public int maxVowels(String s, int k) {
         int n=s.length();
         int count=0;
-        int maxi=Integer.MIN_VALUE;
         for(int i=0;i<k;i++){
-            if(s.charAt(i)=='a'||s.charAt(i)=='e' || s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'){
+            if("aeiouAEIOU".indexOf(s.charAt(i))!= -1){
                 count++;
             }
         }
-        maxi=Math.max(maxi,count);
-        int i=0;
-        int j=k;
-        while(j<n){
-            if(s.charAt(j)=='a'||s.charAt(j)=='e' || s.charAt(j)=='i'||s.charAt(j)=='o'||s.charAt(j)=='u'){
+        int ans=count;
+        for(int i=k;i<n;i++){
+            if("aeiouAEIOU".indexOf(s.charAt(i))!= -1){
                 count++;
             }
-            if(s.charAt(i)=='a'||s.charAt(i)=='e' || s.charAt(i)=='i'||s.charAt(i)=='o'||s.charAt(i)=='u'){
+            if("aeiouAEIOU".indexOf(s.charAt(i-k))!= -1){
                 count--;
             }
-            
-            maxi=Math.max(maxi,count);
-            i++;
-            j++;
+            ans=Math.max(ans,count);
         }
-        return maxi;
+        return ans;
     }
 }
