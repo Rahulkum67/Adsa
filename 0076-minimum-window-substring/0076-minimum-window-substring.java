c3c8dfae32de202fbf1b0dfced8bebfa614fc2b3
Rahul kumar <157143602+Rahulkum67@users.noreply.github.com>
2026-08-20 19:56:54 +0530

    Time: 17 ms (49.48%), Space: 47.1 MB (49.25%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,31 +1,70 @@
 class Solution {
     public String minWindow(String s, String t) {
+        // if(s.length()<t.length()){
+        //     return "";
+        // }
+        // int[] freq=new int[126];
+        // int st=0;
+        // int l=0;
+        // for(int i=0;i<t.length();i++){
+        //     freq[t.charAt(i)]++;
+        // }
+        // int count=t.length();
+        // int minlen=Integer.MAX_VALUE;
+        // for(int i=0;i<s.length();i++){
+        //     char c=s.charAt(i);
+        //     if(freq[c]>0){
+        //         count--;
+        //     }
+        //     freq[c]--;
+        //     while(count==0){
+        //         char ch=s.charAt(l);
+        //         if(i-l+1<minlen){
+        //             minlen=i-l+1;
+        //             st=l;
+        //         }
+        //         freq[ch]++;
+        //         if(freq[ch]>0){
+        //             count++;
+        //         }
+        //         l++;
+        //     }
+        // }
+        // return minlen==Integer.MAX_VALUE ?"": s.substring(st,st+minlen);
         if(s.length()<t.length()){
             return "";
         }
-        int[] freq=new int[126];
+        HashMap<Character,Integer> map=new HashMap<>();
         int st=0;
         int l=0;
         for(int i=0;i<t.length();i++){
-            freq[t.charAt(i)]++;
+            map.put(t.charAt(i),map.getOrDefault(t.charAt(i),0)+1);
         }
         int count=t.length();
         int minlen=Integer.MAX_VALUE;
         for(int i=0;i<s.length();i++){
             char c=s.charAt(i);
-            if(freq[c]>0){
-                count--;
+            if(map.containsKey(c)){
+                if(map.get(c)>0){
+                    count--;
+                }
+                map.put(c,map.get(c)-1);
             }
-            freq[c]--;
+            
             while(count==0){
                 char ch=s.charAt(l);
                 if(i-l+1<minlen){
                     minlen=i-l+1;
                     st=l;
                 }
-                freq[ch]++;
-                if(freq[ch]>0){
-                    count++;
+                if (map.containsKey(ch)) {
+
+                    map.put(ch, map.get(ch) + 1);
+
+                    // Character became missing
+                    if (map.get(ch) > 0) {
+                        count++;
+                    }
                 }
                 l++;
             }
