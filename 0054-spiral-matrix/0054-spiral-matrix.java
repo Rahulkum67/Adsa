f7685f684d54c245a0dd28f5e1be16637bd118d2
Rahul kumar <157143602+Rahulkum67@users.noreply.github.com>
2026-09-12 22:06:01 +0530

    Time: 0 ms (100%), Space: 43 MB (62.29%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,37 +1,33 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> res = new ArrayList<>();
-        
-        int top = 0, bottom = matrix.length - 1;
-        int left = 0, right = matrix[0].length - 1;
-
-        while (top <= bottom && left <= right) {
-            
-            // 1. left → right
-            for (int i = left; i <= right; i++)
-                res.add(matrix[top][i]);
-            top++;
-
-            // 2. top → bottom
-            for (int i = top; i <= bottom; i++)
-                res.add(matrix[i][right]);
-            right--;
-
-            if (top <= bottom) {
-                // 3. right → left
-                for (int i = right; i >= left; i--)
-                    res.add(matrix[bottom][i]);
-                bottom--;
+        List<Integer> ans=new ArrayList<>();
+        int srow=0,erow=matrix.length-1;
+        int scol=0,ecol=matrix[0].length-1;
+        while(srow<=erow && scol<=ecol){
+            for(int i=scol;i<=ecol;i++){
+                ans.add(matrix[srow][i]);
             }
-
-            if (left <= right) {
-                // 4. bottom → top
-                for (int i = bottom; i >= top; i--)
-                    res.add(matrix[i][left]);
-                left++;
+            for(int i=srow+1;i<=erow;i++){
+                
+                ans.add(matrix[i][ecol]);
             }
+            for(int i=ecol-1;i>=scol;i--){
+                if(srow==erow){
+                    break;
+                }
+                ans.add(matrix[erow][i]);
+            }
+            for(int i=erow-1;i>=srow+1;i--){
+                if(scol==ecol){
+                    break;
+                }
+                ans.add(matrix[i][scol]);
+            }
+            srow++;
+            erow--;
+            scol++;
+            ecol--;
         }
-
-        return res;
+        return ans;
     }
 }
